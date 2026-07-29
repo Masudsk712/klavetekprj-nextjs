eb1bc1a1ef72069930052d8cfd4b794b4c839d61
Masud Sk <immasudskz0712@gmail.com>
2026-07-29 13:16:17 +0530

    Create layout architecture

---
+++ src/constants/company.ts
@@ -0,0 +1,15 @@
+export const company = {
+
+name:"Klavetek Green Blocks & Tiles Pvt. Ltd.",
+
+email:"klavetekaacblocks@gmail.com",
+
+phone:"+91 78118 24341",
+
+phone2:"+91 79089 39269",
+
+website:"https://kgbt.in",
+
+address:"Moregram, Madhaipur, Malda, West Bengal 732142"
+
+}
---
+++ src/constants/navigation.ts
@@ -0,0 +1,30 @@
+export const navigation = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Products",
+    href: "/products",
+  },
+  {
+    label: "Gallery",
+    href: "/gallery",
+  },
+  {
+    label: "Blog",
+    href: "/blog",
+  },
+  {
+    label: "Career",
+    href: "/career",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
